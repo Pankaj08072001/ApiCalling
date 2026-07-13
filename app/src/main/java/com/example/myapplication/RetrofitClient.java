598c9b1e2545b17a7f7049b79610308f3ed8357d
Pankaj <pk9278424@gmail.com>
2026-07-13 18:01:02 +0530

    first commit

---
+++ app/src/main/java/com/example/myapplication/RetrofitClient.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+    private static Retrofit retrofit;
+    private static final String BASE_URL = "curl https://reqres.in/api/users?page=2";
+
+    public static Retrofit getRetrofit() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
+
